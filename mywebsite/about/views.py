250a8b98a2b964cabe9a5bd70ed16fbada1dda65
mirzaRakha28 <mirzarakhadwipradana@gmail.com>
2020-05-20 14:44:25 +0700

    #08

--- mywebsite/about/views.py
+++ mywebsite/about/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index (request):
-    return render(request,'about.html')
+    return render(request,'about/index.html')
 
 
 def recent (request):
--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'blog.html')
+    return render(request,'blog/index.html')
 
-def recent (request):
-    return httpResponse('recent')
+# def recent (request):
+#     return httpResponse('recent')
